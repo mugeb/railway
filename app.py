e94b841214f9ccff34509f24a8c21264a1fc04c4
mugeb <oner.begum@gmail.com>
2023-03-17 18:11:24 +0000

    host

--- app.py
+++ app.py
@@ -112,5 +112,7 @@ def update():
 # End webserver stuff
 ########################################
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__":
+#    app.run(debug=True)
+
+if __name__ == "__main__":    app.run(host='0.0.0.0', debug=True, port=5000)

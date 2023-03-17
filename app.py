ce1ff5f506f583edcae60936b1c98223576a9957
mugeb <oner.begum@gmail.com>
2023-03-17 18:47:43 +0000

    host2

--- app.py
+++ app.py
@@ -115,4 +115,5 @@ def update():
 #if __name__ == "__main__":
 #    app.run(debug=True)
 
-if __name__ == "__main__":    app.run(host='0.0.0.0', debug=True, port=5000)
+if __name__ == "__main__":
+        app.run(host='0.0.0.0', debug=True, port=5000)
